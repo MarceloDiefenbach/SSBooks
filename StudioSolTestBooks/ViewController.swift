9957bc07bf9d72161a9fd03af74d1fb6dbf1fe04
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2022-10-27 07:54:41 -0300

    remove unused code

--- StudioSolTestBooks/ViewController.swift
+++ StudioSolTestBooks/ViewController.swift
@@ -13,12 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        if let tabBarItems = self.tabBarController?.tabBar.items {
-            tabBarItems[0].title = "Home"
-            tabBarItems[1].title = "Adicionar"
-            tabBarItems[2].title = "Buscar"
-            tabBarItems[3].title = "Favoritos"
-        }
+        
     }
 }
 
